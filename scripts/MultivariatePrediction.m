6b8286fc4fb194d28304065632a2c744159fbc06
Yuan Chang Leong <ycleong@stanford.edu>
2017-07-16 00:28:28 +0100

    add Multivariate results

--- scripts/MultivariatePrediction.m
+++ scripts/MultivariatePrediction.m
@@ -14,6 +14,7 @@
 %   CANlabCore Toolbox available at https://github.com/canlab/CanlabCore
 %   NifTI toolbox available at 
 %        https://www.mathworks.com/matlabcentral/fileexchange/8797-tools-for-nifti-and-analyze-image
+%   SPM available at http://www.fil.ion.ucl.ac.uk/spm/
 
 clear all
 run_regression = 1;
@@ -24,15 +25,18 @@ font_size = 24;
 %                                             Setup                                               % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Add Toolboxes
-addpath(genpath('../CanlabCore')) 
-addpath(genpath('../NIfTI')) 
+addpath(genpath('../../CanlabCore')) 
+addpath(genpath('../../NIfTI')) 
+addpath(genpath('../spm12'))
+
+% addpath(genpath('/Users/yuanchangleong/spm12'))
 
 % Set Directories 
 dirs.data = '../data';
-dirs.masks = '../masks';
+dirs.mask = '../masks';
 dirs.results = '../results';
 dirs.input = fullfile(dirs.data,'FacesIndegreeFactorCntrlBin_Cntrl4ClosePersNom');
-dirs.output = fullfile(dirs.results,'ConfoundControlled');
+dirs.output = fullfile(dirs.results,'ConfoundControlled_MVPA');
 
 % Make output directory if it doesn't exist
 if ~exist(dirs.output)
@@ -41,8 +45,9 @@ end
 
 % ROI Information 
 mask_files = {'mentalizing.nii','MPFCswath.nii','PrecunPCC.nii',...
-    'RTPJ.nii','LTPJ.nii','RTempPoles.nii','LTempPoles.nii','V1.nii','BilatVS_Plus5Win5_Lose0.nii','brainmask.nii'};
+    'RTPJ.nii','LTPJ.nii','RTempPoles.nii','LTempPoles.nii','BilatVS_Plus5Win5_Lose0.nii','V1.nii'};
 nmask = length(mask_files);
+mask_names = {'Mentalizing','MPFC','PMC','LTP','RTP','LTPJ','RTPJ','Striatum','V1'};
 
 % Subject Information 
 Subjects = load(fullfile(dirs.data,'subject_numbers.txt'));
@@ -131,7 +136,116 @@ if run_regression
         end
         
         save(sprintf('%s.mat',fullfile(dirs.output,mask_files{1,mm}(1:end-4))),...
-            'pred_by_bin','within_sub_corr','stats','explained','cumsum_explained','nPC');
-        
+            'pred_by_bin','within_sub_corr','stats','explained','cumsum_explained','nPC');  
     end
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                Compute Forced-Choice Accuracy                                    % 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Loop over ROIs 
+for rr = 1:length(mask_names)
+    load(sprintf('%s.mat',fullfile(dirs.output,mask_files{1,rr}(1:end-4))));
+
+    % Calculate mean forced choice accuracy
+    FC(rr,1) = mean(pred_by_bin(:,2) > pred_by_bin(:,1)); 
+    FC(rr,2) = mean(pred_by_bin(:,3) > pred_by_bin(:,2)); 
+    FC(rr,3) = mean(pred_by_bin(:,3) > pred_by_bin(:,1)); 
+    
+    % Calcuate SE of forced choice accuracy
+    FC_err(rr,1) = std(pred_by_bin(:,2) > pred_by_bin(:,1))/sqrt(nSub-1);
+    FC_err(rr,2) = std(pred_by_bin(:,3) > pred_by_bin(:,2))/sqrt(nSub-1);
+    FC_err(rr,3) = std(pred_by_bin(:,3) > pred_by_bin(:,1))/sqrt(nSub-1);
+       
+end
+
+% Intialize Figure
+fig = figure();
+hold on
+set(gcf,'Position',[100 100 1000 400]);
+
+% Setup plot
+y = reshape(FC',1,27);
+x = [1,2,3,5,6,7,9,10,11,13,14,15,17,18,19,21,22,23,25,26,27,29,30,31,33,34,35];
+
+% Color for mentalizing ROIs
+bar_col = [203,24,29;   % 3rd
+    252,174,145;        % 1st 
+    251,106,74];        % 2nd
+
+% Color for VS
+bar_col2 = [33,113,181;
+    189,215,231;
+    107,174,214];
+
+% Color for V1
+bar_col3 = [0.5,0.5,0.5;
+    0.9,0.9,0.9;
+    0.7,0.7,0.7];
+
+bar_col = bar_col/255;
+bar_col2 = bar_col2/255;
+
+% Plot mentalizing ROIs
+for i = 1:21
+    b = bar(x(i),y(i),0.7);
+    this_col = mod(i,3)+1;
+    set(b,'facecolor',bar_col(this_col,:))
+end
+
+% Plot VS
+for i = 22:24
+    b = bar(x(i),y(i),0.7);
+    this_col = mod(i,3)+1;
+    set(b,'facecolor',bar_col2(this_col,:))
+end
+
+% Plot V1
+for i = 25:27
+    b = bar(x(i),y(i),0.7);
+    this_col = mod(i,3)+1;
+    set(b,'facecolor',bar_col3(this_col,:))
+end
+
+% Plot error bars
+h = errorbar(x,y,reshape(FC_err',1,27));
+set(h,'Color',[0,0,0],'linestyle','none');
+
+% Chance line
+plot([0,36],[0.5,0.5],'Color','k','LineStyle','--','LineWidth',2);
+
+% Adjust axis
+ylabel('Forced Choice Accuracy');
+set(gca,'xtick',[2,6,10,14,18,22,26,30,34],'xticklabel',mask_names)
+set(gca,'ytick',[0:0.25:1]);
+
+% Run and plot Binomial Test Results
+for rr = 1:length(mask_names)*3
+    p_value(rr) = myBinomTest(y(rr)*nSub,nSub,0.5);
+end
+sig = x(p_value < 0.05);
+scatter(sig,repmat(0.95,1,length(sig)),30,'k','*');
+axis([0 36 0 1]);
+set(gca,'FontSize',20)
+ 
+% Save Figure
+fig_dest = fullfile(dirs.output,'ForcedChoiceAcc');
+set(gcf,'paperpositionmode','auto');
+print(fig,'-depsc',fig_dest);
+
+% Output table:
+row_names = {'Low vs. Mid','Mid vs. High','Low vs. High'};
+T = table(FC, FC_err, 'RowNames',mask_names)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                Compute Within-Subject Correlation                                % 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+all_corr = NaN(nSub,length(mask_names));
+fprintf('Mean Within-Subject Correlation \n')
+for rr = 1:length(mask_names)
+    load(sprintf('%s.mat',fullfile(dirs.output,mask_files{1,rr}(1:end-4))));
+    fprintf('%s: %0.3f (%0.3f) \n',mask_names{rr},mean(within_sub_corr),std(within_sub_corr)/sqrt(length(within_sub_corr)-1));
+end
+
+
+
